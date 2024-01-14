a002b2ac8b6b5505319bb5a1f8255e8d69eb051d
Muhsin-42 <muhsinofficial21@gmail.com>
2024-01-14 10:27:43 +0530

    Vercel Analytics added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
@@ -21,6 +22,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <SubLayout>{children}</SubLayout>
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
