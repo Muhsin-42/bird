ac0f10ff26016dd8539481e0b4e2ea300f2b8e3b
Muhsin-42 <muhsinofficial21@gmail.com>
2024-02-06 18:24:17 +0530

    resolved hydration issue

--- components/shared/ListPosts.tsx
+++ components/shared/ListPosts.tsx
@@ -1,9 +1,6 @@
-"use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PostCard from "../cards/PostCard/PostCard";
 import { IPostCard } from "@/interfaces/propInterfaces";
-import { fetchPosts } from "@/lib/actions/thread.actions";
-import useLoading from "@/hooks/useLoading";
 
 interface IListPosts {
   posts: IPostCard[];
@@ -11,45 +8,13 @@ interface IListPosts {
 }
 
 const ListPosts = ({ posts, currentUserId }: IListPosts) => {
-  const [page, setPage] = useState(2);
-  const [hasMore, setHasMore] = useState(true);
-  const [allPosts, setAllPosts] = useState(posts);
-  const { isLoading, setIsLoading } = useLoading();
-  useEffect(() => {
-    const loadMorePosts = async () => {
-      if (!hasMore) return;
-
-      setIsLoading(true);
-      const result = await fetchPosts(page, 20);
-      setIsLoading(false);
-      if (result && result.isNext) {
-        setAllPosts([...allPosts, ...result.posts]);
-        setPage((prevPage) => prevPage + 1);
-      } else {
-        setHasMore(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-
-    function handleScroll() {
-      if (
-        window.innerHeight + document.documentElement.scrollTop !==
-        document.documentElement.offsetHeight
-      )
-        return;
-      loadMorePosts();
-    }
-  }, [page, hasMore, allPosts]);
-
   return (
     <section className="mt-9 flex flex-col ">
-      {allPosts?.length === 0 ? (
+      {posts?.length === 0 ? (
         <p className="no-result">No threads found</p>
       ) : (
-        <>
-          {allPosts?.map((post: any) => (
+        <div>
+          {posts?.map((post: any) => (
             <div className="border-b border-dark-4" key={post?._id}>
               <PostCard
                 key={post?._id}
@@ -68,10 +33,7 @@ const ListPosts = ({ posts, currentUserId }: IListPosts) => {
               />
             </div>
           ))}
-        </>
-      )}
-      {isLoading && (
-        <p className="animate-pulse text-center text-gray-1">Loading...</p>
+        </div>
       )}
     </section>
   );
